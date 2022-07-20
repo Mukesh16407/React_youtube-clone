17db0fdd8d7333a6afb352f21a0e1a63b9d548f0
MUKESH KUMAR <66511917+Mukesh16407@users.noreply.github.com>
2022-07-20 18:47:17 +0530

    change Background Color

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -9,13 +9,14 @@ align-items: center;
 justify-content: center;
 height: calc(100vh -56px);
 color: ${({theme})=>theme.text};
+
 `;
 
 const Wrapper = styled.div`
 display: flex;
 align-items: center;
 flex-direction: column;
-background-color: ${({theme})=>theme.bgLight};
+background-color: ${({theme})=>theme.bgLighter};
 padding: 20px 50px;
 border:1px solid ${({theme})=>theme.soft};
 `;
